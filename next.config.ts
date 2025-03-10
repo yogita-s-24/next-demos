3fe127b8f595094fcd5fa8674116575990b4421a
Yogita Shete <yogitashete24@gmail.com>
2025-03-10 13:13:56 +0530

    configured Next.js image domains to allow external profile images.

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'randomuser.me', 'avatars.githubusercontent.com']
   },
 };
 
